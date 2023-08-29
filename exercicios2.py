24e41a6f9435a96f34a167f550f52df02a56d347
Ualace Araujo <88940821+UalaceAraujo@users.noreply.github.com>
2023-08-29 20:15:26 -0300

    first commit

---
+++ exercicios2.py
@@ -0,0 +1,11 @@
+# soma=0
+# contador=0
+# for numero in range(10,51,2):
+#     if numero % 2==0:
+#         soma += numero
+#         contador += 1
+#         if contador >0:
+#             media= soma/contador 
+#             print('A media aritimeticas dos numeros pares entre 10 e 50 é:',media)
+#         else:
+#           print (" Nao há numeros pares entre 10 e 50")
